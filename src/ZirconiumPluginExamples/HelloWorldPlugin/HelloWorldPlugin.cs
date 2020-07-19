0f1534006571fad3785d5553a7f8107a41b90be1
ChronosX88 <chronosx88@gmail.com>
2020-07-19 14:47:38 +0400

    Add example of hello world plugin

---
+++ src/ZirconiumPluginExamples/HelloWorldPlugin/HelloWorldPlugin.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Zirconium.Core.Logging;
+using Zirconium.Core.Models;
+using Zirconium.Core.Modules.Interfaces;
+
+namespace HelloWorldPlugin
+{
+    internal class HelloWorldPlugin : IModuleAPI
+    {
+        public string GetModuleUniqueName() => "HelloWorldPlugin";
+
+        public void Initialize(IHostModuleAPI hostModuleAPI)
+        {
+            var handler = new C2SHandler(hostModuleAPI);
+            hostModuleAPI.Hook(handler);
+            Log.Debug("plugin is initialized");
+        }
+    }
+
+    internal class C2SHandler : IC2SMessageHandler
+    {
+        private IHostModuleAPI hostModuleAPI;
+
+        public C2SHandler(IHostModuleAPI hostModuleAPI) {
+            this.hostModuleAPI = hostModuleAPI;
+        }
+
+        public string GetHandlerUniqueID() => "test";
+
+        public string GetHandlingMessageType() => "test";
+
+        public void HandleMessage(ConnectionInfo connInfo, BaseMessage message)
+        {
+            BaseMessage msg = new BaseMessage(message, true);
+            msg.Payload["testProp"] = "hello world";
+            msg.Ok = true;
+            msg.From = hostModuleAPI.GetServerID();
+            string strMsg = JsonConvert.SerializeObject(msg);
+            connInfo.ConnectionHandler.SendMessage(strMsg);
+        }
+
+        public bool IsAuthorizationRequired() => false;
+    }
+}
