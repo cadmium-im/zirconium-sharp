370a7e8dc24d31e2021fb3190e65ba9cb611f36a
ChronosX88 <chronosx88@gmail.com>
2020-10-07 22:18:43 +0400

    Make PreInitialize method optional for implementing by plugins

--- src/Zirconium/Core/Plugins/Interfaces/IPluginAPI.cs
+++ src/Zirconium/Core/Plugins/Interfaces/IPluginAPI.cs
@@ -5,7 +5,7 @@ namespace Zirconium.Core.Plugins.Interfaces
     public interface IPluginAPI
     {
         void Initialize(IPluginHostAPI hostModuleAPI);
-        void PreInitialize(IPluginManager pluginManager);
+        void PreInitialize(IPluginManager pluginManager) {}
         string GetPluginUniqueName();
     }
 }
