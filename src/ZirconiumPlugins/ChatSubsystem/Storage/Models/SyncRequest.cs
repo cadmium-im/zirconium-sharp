2e72d8377e915866af201fd0657b2dddead23db9
ChronosX88 <chronosx88@gmail.com>
2021-03-25 14:29:30 +0300

    Add JsonProperty to Sync Request/Response model

--- src/ZirconiumPlugins/ChatSubsystem/Storage/Models/SyncRequest.cs
+++ src/ZirconiumPlugins/ChatSubsystem/Storage/Models/SyncRequest.cs
@@ -1,12 +1,20 @@
+using Newtonsoft.Json;
 using Zirconium.Core.Models;
 
 namespace ChatSubsystem.Storage.Models
 {
     public class SyncRequest
     {
+        [JsonProperty("since")]
         public EntityID Since { get; set; }
+        
+        [JsonProperty("limit")]
         public int Limit { get; set; }
+        
+        [JsonProperty("chatID")]
         public EntityID ChatID { get; set; }
+        
+        [JsonProperty("syncDirection")]
         public string SyncDirection { get; set; }
     }
 }
--- src/ZirconiumPlugins/ChatSubsystem/Storage/Models/SyncResponse.cs
+++ src/ZirconiumPlugins/ChatSubsystem/Storage/Models/SyncResponse.cs
@@ -1,12 +1,18 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Zirconium.Core.Models;
 
 namespace ChatSubsystem.Storage.Models
 {
     public class SyncResponse
     {
+        [JsonProperty("nextBatch")]
         public EntityID NextBatch { get; set; }
+        
+        [JsonProperty("mentionedChats")]
         public IList<Chat> MentionedChats { get; set; }
+        
+        [JsonProperty("events")]
         public IList<Event> Events { get; set; }
     }
 }
